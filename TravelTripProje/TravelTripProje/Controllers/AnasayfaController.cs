8f9338c289f66eb456215e0ec8babfd85b867ea9
cenap varol <varolcenap@gmail.com>
2022-01-02 01:19:05 +0300

    CodeFirstIslemleriYapildi

--- TravelTripProje/TravelTripProje/Controllers/AnasayfaController.cs
+++ TravelTripProje/TravelTripProje/Controllers/AnasayfaController.cs
@@ -13,11 +13,6 @@ namespace TravelTripProje.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            return View();
-        }
     }
 
 

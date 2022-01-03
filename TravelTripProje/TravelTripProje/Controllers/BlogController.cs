bb04cacfc695f0a33edfad8db9d6a0188bd93eea
cenap varol <varolcenap@gmail.com>
2022-01-03 09:26:45 +0300

    Tablolar oluşturludu ve blog sayfası eklendi

---
+++ TravelTripProje/TravelTripProje/Controllers/BlogController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+
+
+        Context context = new Context();
+        public ActionResult Index()
+        {
+            var deger = context.Blogs.ToList();
+            return View(deger);
+        }
+
+        BlogYorum by = new BlogYorum();
+        public ActionResult BlogDetay(int id)
+        {
+            //var deger = context.Blogs.Where(x => x.ID == id).ToList();
+            by.Deneme1 = context.Blogs.Where(x => x.ID == id).ToList();
+            by.Deneme2 = context.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View(by);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Models/Siniflar/BlogYorum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TravelTripProje.Models.Siniflar
+{
+    public class BlogYorum
+    {
+        public IEnumerable<Blog> Deneme1 { get; set; }
+        public IEnumerable<Yorumlar> Deneme2 { get; set; }
+    }
+}
